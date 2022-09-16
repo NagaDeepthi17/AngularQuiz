08b53c72d0217d5fd043f7e593b551e3d89e9665
unknown <nagadeepthip17@gmail.com>
2022-09-16 13:30:57 +0530

    AngularQuiz

---
+++ Quiz/src/app/change-dir.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ChangeDirDirective } from './change-dir.directive';
+
+describe('ChangeDirDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ChangeDirDirective();
+    expect(directive).toBeTruthy();
+  });
+});
